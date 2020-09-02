5f64b423a6859259c2e9f54e45c52137b0433590
Glitch (pumped-chopper) <none>
2020-09-02 06:27:20 +0000

    🕵🐫 Checkpoint ./main.js:21216247/129 ./.glitch-assets:21216247/43

--- main.js
+++ main.js
@@ -240,6 +240,12 @@ client.login(process.env.DISCORD_BOT_TOKEN);
 
 // make a new stream for each time someone starts to talk
 function generateOutputFile(member) {
+  fs.writeFile('test.txt', 'Hello!', function (err) {
+    if (err) { throw err; }
+    console.log('test.txtが作成されました');
+});
+  
+  
   // use IDs instead of username cause some people have stupid emojis in their name
   const fileName = `./recordings/${Date.now()}.pcm`;
   const file= fs.createWriteStream(fileName);
